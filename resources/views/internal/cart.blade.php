190f337e2e32e615e1b5367840834b98bda19ae1
Dominik Dąbrowski <86299305+xdomsky@users.noreply.github.com>
2022-02-13 23:26:47 +0100

    cart

--- resources/views/internal/cart.blade.php
+++ resources/views/internal/cart.blade.php
@@ -1,42 +1,17 @@
 @include('navi')
 <link href="{{ asset('css/cart.css') }}" media="all" rel="stylesheet" type="text/css" />
-<!-- <div class="content">
-<div class="row">
-    @if( $cart )
-        @if( $cart->item1Quantity > 0)
-        <div class="box-element">
-            <img src="{{ $product1->productImg }}" alt="Stół" width="100px"/>
-            <h3>Nazwa:</h3><p>{{ $product1->productName }}</p>
-            <h3>Cena za szt:</h3><p>{{ $product1->productPrice }}</p>
-            <h3>Ilosc:</h3><p>{{ $cart->item1Quantity }}</p>
-            <h3>Cena koncowa:</h3><p>{{ $product1->productPrice*$cart->item1Quantity }}</p>
-        </div>
-        @endif
-        @if( $cart->item2Quantity > 0)
-        <div class="box-element">
-            <img src="{{ $product2->productImg }}" alt="Stół" width="100px">
-            <h3>Nazwa:</h3><p>{{ $product2->productName }}</p>
-            <h3>Cena za szt:</h3><p>{{ $product2->productPrice }}</p>
-            <h3>Ilosc:</h3><p>{{ $cart->item2Quantity }}</p>
-            <h3>Cena koncowa:</h3><p>{{ $product2->productPrice*$cart->item2Quantity }}</p>
-        </div>
-        @endif
-    @endif
-</div>
-</div> -->
-
 <div class="content">
     <div class="row">
         <div class="box-element">
             <div class="shop-btn">
-                <a href="/">&#x2190; Kontynuj zakupy</a>
+                <div class="space-btn btn-wd"><a href="/">&#x2190; Kontynuj zakupy</a></div>
             </div>
             <br>
             <hr>
             <br>
             <div class="space">
                 <div>
-                    <h5>Produkty: <strong>            
+                    <h5>Produkty: <strong>
                         @if($cart)
                         {{ $cart->item1Quantity+$cart->item2Quantity }}
                         @else
@@ -45,7 +20,7 @@
         </strong></h5>
                 </div>
                 <div>
-                    <h5>Razem: <strong><span name="final-price">{{ $product1->productPrice }}</span> PLN</strong></h5>
+                    <h5>Razem: <strong><span name="final-price">{{ ($product1->productPrice*$cart->item1Quantity)+($product2->productPrice*$cart->item2Quantity)  }}</span> PLN</strong></h5>
                 </div>
                 <div class="space-btn"><a>Potwierdzenie</a></div>
             </div>
@@ -64,7 +39,7 @@
             </div>
 
             <div class="cart-row">
-                <div style="flex:2"><img src="{{ $product1->productImg }}" alt="Stół" width="100px"/></div>
+                <div style="flex:2"><img src="{{ $product1->productImg }}" alt="Stół" class="row-image"/></div>
                 <div style="flex:2">
                     <p>{{ $product1->productName }}</p>
                 </div>
@@ -74,12 +49,12 @@
                 <div style="flex:1">
                     <p class="quantity" id="qua">{{ $cart->item1Quantity }}</p>
                     <div class="quantity">
-                        <img class="chg-quantity" src="{{asset('img/arrow_up.png')}}" id="qua-up">
-                        <img class=" chg-quantity" src="{{asset('img/arrow_down.png')}}" id="qua-down">
+                        <img class="chg-quantity" src="{{asset('img/arrow_up.png')}}" id="addItem1">
+                        <img class=" chg-quantity" src="{{asset('img/arrow_down.png')}}" id="remItem1">
                     </div>
                 </div>
                 <div style=" flex:1">
-                    <p><span name="final-price">{{ $product1->productPrice }}</span> PLN</p>
+                    <p><span name="final-price">{{ $product1->productPrice*$cart->item1Quantity }}</span> PLN</p>
                 </div>
             </div>
             </div>
@@ -96,7 +71,7 @@
             </div>
 
             <div class="cart-row">
-                <div style="flex:2"><img src="{{ $product2->productImg }}" alt="Stół" width="100px"/></div>
+                <div style="flex:2"><img src="{{ $product2->productImg }}" alt="Stół" class="row-image"/></div>
                 <div style="flex:2">
                     <p>{{ $product2->productName }}</p>
                 </div>
@@ -106,12 +81,12 @@
                 <div style="flex:1">
                     <p class="quantity" id="qua">{{ $cart->item2Quantity }}</p>
                     <div class="quantity">
-                        <img class="chg-quantity" src="{{asset('img/arrow_up.png')}}" id="qua-up">
-                        <img class=" chg-quantity" src="{{asset('img/arrow_down.png')}}" id="qua-down">
+                        <img class="chg-quantity" src="{{asset('img/arrow_up.png')}}" id="addItem2">
+                        <img class=" chg-quantity" src="{{asset('img/arrow_down.png')}}" id="remItem2">
                     </div>
                 </div>
                 <div style=" flex:1">
-                    <p><span name="final-price">{{ $product2->productPrice }}</span> PLN</p>
+                    <p><span name="final-price">{{ $product2->productPrice*$cart->item2Quantity }}</span> PLN</p>
                 </div>
             </div>
         @endif
